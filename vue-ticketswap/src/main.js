9d2917a25cc302282d3e4159cf32fa322b114ebf
Caleb Liu <calebliu@Calebs-MacBook-Pro.local>
2023-02-26 18:05:45 -0600

    Initial signup functionality

--- vue-ticketswap/src/main.js
+++ vue-ticketswap/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+
+createApp(App).use(router, axios).mount('#app')
 
 const app = createApp(App);
 app.use(router);
