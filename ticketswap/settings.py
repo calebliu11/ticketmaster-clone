cb373baac12189d7aaaefa7019c6c2098fe87b03
Caleb Liu <calebliu@Calebs-MacBook-Pro.local>
2023-03-18 16:45:42 -0500

    Changed allowed hosts

--- ticketswap/settings.py
+++ ticketswap/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = "django-insecure-kpt3en8cjrsi)vunwfco1p*y1f6*qwrh(hwq4o4krnpe3k$w&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1'
-    ]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
