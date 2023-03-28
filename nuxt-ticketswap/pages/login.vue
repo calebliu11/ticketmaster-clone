e9f763a50a74da42e62985f63d0d45c14ff24a81
Caleb Liu <calebliu@calebs-mbp.dhcp.wustl.edu>
2023-03-28 10:33:28 -0500

    fixing login bugs

--- nuxt-ticketswap/pages/login.vue
+++ nuxt-ticketswap/pages/login.vue
@@ -18,12 +18,12 @@
                         <input type="text" class="input" v-model="password">
                     </div> -->
                     <div class="field has-addons">
-                        <div class="control is-expanded">
+                        <div class="    ">
                             <input v-if="showPassword" type="text" class="input" v-model="password" />
                             <input v-else type="password" class="input" v-model="password">
                         </div>
                         <div class="control">
-                            <button class="button" @click="toggleShow" :style="{backgroundColor: show ? 'white' : '#00d1b2', color: show ? 'black' : 'white'}">
+                            <button class="button" type="button" @click="toggleShow" :style="{backgroundColor: show ? 'white' : '#00d1b2', color: show ? 'black' : 'white'}">
                                 Show
                             </button>
                         </div>
@@ -88,10 +88,8 @@ export default {
                     this.$router.push('/')
                 })
                 .catch(error => {
+                    this.errors.push(error.response._data.non_field_errors)
                     if (error.response) {
-                        for (const property in error.response) {
-                            this.errors.push(`${property}: ${error.response[property]}`)
-                        }
                         console.log(JSON.stringify(error.response))
 
                     }
